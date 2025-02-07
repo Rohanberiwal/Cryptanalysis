3e736e96ed27b783664b02b48645976c037623b2
lèòuhànbèilǐwǎlè <119040957+Rohanberiwal@users.noreply.github.com>
2025-02-07 12:25:27 +0530

    Update Part1.py
    toy cipher  for rounds change ,

--- Assignment 3/Part1.py
+++ Assignment 3/Part1.py
@@ -1,39 +1,28 @@
-import random
-
-S_BOX = [0x6, 0x4, 0xC, 0x5, 0x0, 0x7, 0x2, 0xE, 0x1, 0xF, 0x3, 0xD, 0x8, 0xA, 0x9, 0xB]
+S = [6, 4, 12, 5, 0, 7, 2, 14, 1, 15, 3, 13, 8, 10, 9, 11]
 P = [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15]
 
-def substitute(nibble):
-    return S_BOX[nibble]
+def toy_cipher_encrypt(m, round_keys):
+    u = m
+    r = 5
 
-def permute(bits):
-    return [bits[P[i]] for i in range(16)]
+    for i in range(1, r):
+        a = u ^ round_keys[i - 1]
+        A0, A1, A2, A3 = (a >> 12) & 0xF, (a >> 8) & 0xF, (a >> 4) & 0xF, a & 0xF
+        S_output = (S[A0] << 12) | (S[A1] << 8) | (S[A2] << 4) | S[A3]
+        y = S_output
+        permuted_y = 0
+        for j in range(16):
+            permuted_y |= ((y >> j) & 1) << P[j]
+        u = permuted_y
 
-def to_bits(val, size=16):
-    return [(val >> i) & 1 for i in range(size - 1, -1, -1)]
+    a = u ^ round_keys[r - 1]
+    A0, A1, A2, A3 = (a >> 12) & 0xF, (a >> 8) & 0xF, (a >> 4) & 0xF, a & 0xF
+    S_output = (S[A0] << 12) | (S[A1] << 8) | (S[A2] << 4) | S[A3]
+    y = S_output
+    ciphertext = y ^ round_keys[r - 1]
+    return ciphertext
 
-def from_bits(bits):
-    return sum(b << (15 - i) for i, b in enumerate(bits))
-
-
-def encrypt(plaintext, round_keys):
-    state = plaintext
-    for i in range(len(round_keys) - 1):
-        state ^= round_keys[i]
-        state_nibbles = [(state >> (4 * j)) & 0xF for j in range(4)][::-1]
-        state_nibbles = [substitute(n) for n in state_nibbles]
-        state_bits = to_bits(sum(n << (4 * j) for j, n in enumerate(state_nibbles)))
-        state_bits = permute(state_bits)
-        state = from_bits(state_bits)
-
-    state ^= round_keys[-1]
-    state_nibbles = [(state >> (4 * j)) & 0xF for j in range(4)][::-1]
-    state_nibbles = [substitute(n) for n in state_nibbles]
-    state = sum(n << (4 * j) for j, n in enumerate(state_nibbles))
-    
-    return state ^ round_keys[-1]
-
-plaintext = 0x1234
-round_keys = [0x1111, 0x2222, 0x3333, 0x4444, 0x5555]
-ciphertext = encrypt(plaintext, round_keys)
-print(f"Ciphertext: {ciphertext:04X}")
+message = 0x1234
+round_keys = [0x1111, 0x2222, 0x3333, 0x4444, 0x5555]  
+ciphertext = toy_cipher_encrypt(message, round_keys)
+print(f"Ciphertext: {ciphertext:016x}")
